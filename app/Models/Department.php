b077c1554763d477aac1eaa078ca153e2daf1ccb
Diego Santana <3c.santana@gmail.com>
2026-04-18 18:42:15 -0300

    feat: Define os relacionamentos no model Department

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['name'])]
 class Department extends Model
 {
-    //
+    /**
+     * Um departamento possui vários usuários
+     */
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    /**
+     * Um departamento possui vários candidatos
+     */
+    public function candidates()
+    {
+        return $this->hasMany(Candidate::class);
+    }
 }
