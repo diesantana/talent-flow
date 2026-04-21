3974072ef2e57c3b11e331d5f7b0bbfec31017dc
Diego Santana <3c.santana@gmail.com>
2026-04-21 18:14:51 -0300

    feat: Configura a rota inicial para user autenticados

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rota inicial para usuários logados
+Route::get('/candidates', function () {
+    return view('candidates.index'); // Implementação temporária. (O controler será implementado posteriormente)
+})->middleware(['auth'])->name('candidates.index');
