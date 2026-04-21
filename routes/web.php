c4fb372094bc5da4b1ef7d6d8c0e8881e71e413e
Diego Santana <3c.santana@gmail.com>
2026-04-21 19:08:03 -0300

    feat: Configura a rota para o perfil o user autenticado

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,9 @@ Route::get('/', function () {
 Route::get('/candidates', function () {
     return view('candidates.index'); // Implementação temporária. (O controler será implementado posteriormente)
 })->middleware(['auth'])->name('candidates.index');
+
+// Rota para o perfil do usuário autenticado
+Route::get('/profile', function () {
+    return view('profile.show');
+})->middleware(['auth'])->name('profile.show');
+
